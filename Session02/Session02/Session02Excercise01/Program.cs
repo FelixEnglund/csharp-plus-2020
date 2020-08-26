7b8b4459be65b303f0a67a8e3c7f4238a031540c
LAPTOP-7VK8KFSA\felix.englund.1892 <felix.englund@studerande.plushogskolan.se>
2020-08-26 10:09:39 +0200

    testing integers

--- Session02/Session02/Session02Excercise01/Program.cs
+++ Session02/Session02/Session02Excercise01/Program.cs
@@ -7,6 +7,12 @@ namespace Session02Excercise01
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var integer = 0;
+
+            Console.WriteLine("Integer is " + integer.ToString());
+
+
         }
     }
 }
